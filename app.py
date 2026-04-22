fd4126a31c73463cfe68e79d92f313a272a64eaa
drsofa-max <drsofa@gmail.com>
2026-04-22 14:25:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def home():
     return jsonify({'message': 'API running'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    port = int(os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
